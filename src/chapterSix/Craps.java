da7f81881ffb411d165a310595ece58fc82aed04
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-09-19 21:49:25 +0100

    new addition

--- src/chapterSix/Craps.java
+++ src/chapterSix/Craps.java
@@ -1,2 +1,69 @@
-package chapterSix;public class Craps {
+package chapterSix;
+
+import java.security.SecureRandom;
+
+public class Craps {
+
+    private static final SecureRandom randomNumber = new SecureRandom();
+
+    private enum Status {WON , LOST , CONTINUE};
+
+    private static final int SNAKE_EYES =2;
+    private static final int Trey = 3;
+    private static final int SEVEN = 7;
+    private static final int YO_LEVEN = 11;
+    private static final int BOX_CARS = 12;
+
+    public static void main(String[] args) {
+        int myPoint = 0;
+        Status gameStatus;
+        int sumOfDice = rollDice();
+
+        switch(sumOfDice){
+
+            case SEVEN:
+            case YO_LEVEN:
+                gameStatus = Status.WON;
+                break;
+            case SNAKE_EYES:
+            case Trey:
+            case BOX_CARS:
+                gameStatus = Status.LOST;
+                break;
+            default:
+                gameStatus = Status.CONTINUE;
+                myPoint = sumOfDice;
+                System.out.printf("point is %d%n", myPoint);
+                break;
+        }
+        while (gameStatus == Status.CONTINUE){
+
+            sumOfDice = rollDice();
+
+            if (sumOfDice == myPoint)
+                gameStatus = Status.WON;
+
+            else
+                if (sumOfDice == SEVEN)
+                    gameStatus = Status.LOST;
+
+        }
+        if (gameStatus == Status.WON)
+            System.out.println("player wins");
+        else
+            System.out.println("player loses");
+    }
+
+    private static int rollDice() {
+
+        int die1 = 1 + randomNumber.nextInt(6);
+        int die2 = 1 + randomNumber.nextInt(6);
+
+        int sum = die1 + die2;
+
+        System.out.printf("Player rolled %d + %d = %d%n",
+                die1,die2,sum);
+
+        return sum;
+    }
 }
