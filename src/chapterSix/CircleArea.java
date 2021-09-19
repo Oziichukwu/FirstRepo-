bad5c83b6a88e4e257df7173197ed78726898a03
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-09-19 21:32:47 +0100

    updated file

--- src/chapterSix/CircleArea.java
+++ src/chapterSix/CircleArea.java
@@ -1,2 +1,17 @@
-package chapterSix;public class CircleArea {
+package chapterSix;
+
+import java.util.Scanner;
+
+public class CircleArea {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the radius of the circle");
+        double radius = input.nextInt();
+        System.out.printf("%s%.2f","The area of the circle is ",areaOfACircle(radius));
+    }
+
+    private static double areaOfACircle(double radius){
+        return Math.PI * radius * radius;
+    }
 }
