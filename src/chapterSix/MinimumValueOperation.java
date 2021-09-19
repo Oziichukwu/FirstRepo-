b10012d4a1eb4a4421b5db295023306377dd63a9
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-09-19 22:19:54 +0100

    updated

--- src/chapterSix/MinimumValueOperation.java
+++ src/chapterSix/MinimumValueOperation.java
@@ -1,14 +1,24 @@
 package chapterSix;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class MinimumValueOperation {
-    public static void main(String[] args) {
-
-
-    }
-
-    private static void inputCollectionMethod(){
-
-
+    public static void main(String... args) {
+        Scanner input = new Scanner(System.in);
+        try{
+        System.out.println("Enter first Number");
+        double firstNumber = input.nextDouble();
+        System.out.println("Enter second Number");
+        double secondNumber = input.nextDouble();
+        System.out.println("Enter third Number");
+        double thirdNumber = input.nextDouble();
+        System.out.printf("%s%.2f","The minimum value is ", calculateMinimumValue(firstNumber,secondNumber,thirdNumber));
+        }
+        catch (InputMismatchException ep){
+            System.out.printf("Exception: %s%n", "invalid input");
+            main();
+        }
     }
 
     private static double calculateMinimumValue(double firstNumber, double secondNumber,
