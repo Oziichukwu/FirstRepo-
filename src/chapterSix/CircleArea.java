ecdc6ad626fc448942b751d347f2e5d75ec3b107
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-09-19 21:40:12 +0100

    update again

--- src/chapterSix/CircleArea.java
+++ src/chapterSix/CircleArea.java
@@ -1,14 +1,21 @@
 package chapterSix;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class CircleArea {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner input = new Scanner(System.in);
-
+        try{
         System.out.println("Enter the radius of the circle");
         double radius = input.nextInt();
         System.out.printf("%s%.2f","The area of the circle is ",areaOfACircle(radius));
+        }
+        catch (InputMismatchException ex){
+            System.out.printf("Exception: %s%n", "Invalid input, radius must be an integer or double");
+            System.out.println();
+        main();
+        }
     }
 
     private static double areaOfACircle(double radius){
