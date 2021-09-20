7bbede553a714556785217cb813c5405f43b936d
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-09-20 05:45:27 +0100

    minimum value

--- src/chapterSix/MinimumValueOperation.java
+++ src/chapterSix/MinimumValueOperation.java
@@ -5,21 +5,47 @@ import java.util.Scanner;
 
 public class MinimumValueOperation {
     public static void main(String... args) {
+        double firstNumber = collectInput("Enter first Number");
+        double secondNumber = collectInputTwo("Enter second Number");
+        double thirdNumber = collectInputThree("Enter third number");
+        System.out.printf("The minimum is %.2f", calculateMinimumValue(firstNumber, secondNumber, thirdNumber));
+    }
+
+    private static double collectInput(String prompt) {
+
         Scanner input = new Scanner(System.in);
-        try{
-        System.out.println("Enter first Number");
-        double firstNumber = input.nextDouble();
-        System.out.println("Enter second Number");
-        double secondNumber = input.nextDouble();
-        System.out.println("Enter third Number");
-        double thirdNumber = input.nextDouble();
-        System.out.printf("%s%.2f","The minimum value is ", calculateMinimumValue(firstNumber,secondNumber,thirdNumber));
-        }
-        catch (InputMismatchException ep){
-            System.out.printf("Exception: %s%n", "invalid input");
-            main();
+        try {
+            System.out.println("Enter first Number");
+            return input.nextDouble();
+        } catch (InputMismatchException ep) {
+            System.out.printf("Exception: %s%n", "invalid input, kindly reenter a valid input");
+            return collectInput(prompt);
         }
     }
+    private static double collectInputTwo(String promptTwo){
+        Scanner input = new Scanner(System.in);
+        try {
+            System.out.println("Enter second Number");
+            return input.nextDouble();
+        }
+        catch (InputMismatchException ex){
+            System.out.printf("Exception: %s%n", "invalid input, kindly reenter a valid input");
+            return collectInputTwo(promptTwo);
+        }
+    }
+    private static double collectInputThree(String promptThree){
+        Scanner input = new Scanner(System.in);
+        try {
+            System.out.println("Enter third Number");
+            return input.nextDouble();
+        }
+        catch (InputMismatchException ex){
+            System.out.printf("Exception: %s%n", "invalid input, kindly reenter a valid input");
+            return collectInputThree(promptThree);
+        }
+
+    }
+
 
     private static double calculateMinimumValue(double firstNumber, double secondNumber,
                                              double thirdNumber){
