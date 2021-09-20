1d584411460e2dd3ed8cce1e98aa3b5f0fd65e27
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-09-20 14:13:24 +0100

    updated

--- src/chapterSix/EvenOrOddNumberSelection.java
+++ src/chapterSix/EvenOrOddNumberSelection.java
@@ -1,14 +1,20 @@
 package chapterSix;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class EvenOrOddNumberSelection {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner input = new Scanner(System.in);
-
+        try {
         System.out.println("Enter a number to determine if it is even or odd");
         int number = input.nextInt();
         System.out.println(toDetermineWhetherAnIntegerIsEvenOrOdd(number));
+        }
+        catch (InputMismatchException ex){
+            System.out.printf("Exception: %s%n", "invalid input");
+            main();
+        }
     }
 
     private static boolean toDetermineWhetherAnIntegerIsEvenOrOdd(int number){
