e4adc7e3dabff03453bdbcf5d5bec22603a26e12
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-09-20 23:45:17 +0100

    updated files

--- src/eStoreCheckout/Cart.java
+++ src/eStoreCheckout/Cart.java
@@ -1,2 +1,40 @@
-package eStoreCheckout;public class cart {
+package eStoreCheckout;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class Cart {
+
+    private final List<Items>collectionOfItems;
+
+    public Cart() {
+        collectionOfItems = new ArrayList<>();
+    }
+
+    public void addItemsToCart(Items items){
+        collectionOfItems.add(items);
+    }
+
+    public BigDecimal calculateTotalOfItemsInCart(){
+       BigDecimal itemsTotal = BigDecimal.ZERO;
+
+       for (Items items : collectionOfItems){
+           itemsTotal = itemsTotal.add(calculateTotalOfItemsInCart());
+       }
+        return itemsTotal;
+    }
+
+    public List<Items> getItemsInCart() {
+        return Collections.unmodifiableList(collectionOfItems);
+    }
+
+    public BigDecimal calculateVatOfTotalOfItems(){
+        BigDecimal  VAT = BigDecimal.valueOf(7.5);
+        BigDecimal  percent = BigDecimal.valueOf(100.00);
+        return  calculateTotalOfItemsInCart().multiply(VAT).divide(percent);
+    }
+
 }
--- src/eStoreCheckout/Items.java
+++ src/eStoreCheckout/Items.java
@@ -1,2 +1,26 @@
-package eStoreCheckout;public class items {
+package eStoreCheckout;
+
+import java.math.BigDecimal;
+
+public class Items {
+    private String product;
+    private BigDecimal pricePerUnit;
+    private int quantity;
+
+
+    public Items(String product, BigDecimal pricePerUnit, int quantity){
+        this.product = product;
+        this.pricePerUnit = pricePerUnit;
+        this.quantity = quantity;
+    }
+
+    public BigDecimal calculateTotalForItems(){
+
+        return pricePerUnit.multiply(BigDecimal.valueOf(quantity));
+    }
+
+    public String toString(){
+     return  String.format("%-15s%-15.2f%-10d%-10.2f", product, pricePerUnit, quantity,
+             calculateTotalForItems());
+    }
 }
