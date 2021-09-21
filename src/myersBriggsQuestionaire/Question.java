847aa324ee54868328cc81ac98a9f6a90a5fa8f8
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-09-21 22:54:50 +0100

    myers code

---
+++ src/myersBriggsQuestionaire/Question.java
@@ -0,0 +1,46 @@
+package myersBriggsQuestionaire;
+
+public class Question {
+
+    private char response;
+
+    String[] options = new String[20];
+
+//    public Question(String[] options, char response) {
+//        this.response = response;
+//        new Question(new String[]{"A: expand energy, enjoy groups", "B: conserve energy, enjoy one-one"});
+//    }
+
+    public Question(String[] options) {
+   new Question(new String[]{"A: expand energy, enjoy groups", "B: conserve energy, enjoy one-one"});
+   new Question(new String[]{"A:  interpret literally", "B: look for meaning and possibilities"});
+   new Question(new String[]{"A:  logical,thinking,questioning", "B: empathetic, feeling, accommodating"});
+   new Question(new String[]{"A:  organized, orderly", "B: flexible, adaptable"});
+   new Question(new String[]{"A:  more outgoing, think out loud", "B: more reserved, think to yourself"});
+   new Question(new String[]{"A:  practical, realistic, experiential", "B: imaginative, innovative, theoretical"});
+   new Question(new String[]{"A:  candid, straight forward, frank", "B: tactful, kind, encouraging"});
+   new Question(new String[]{"A:   plan, schedule", "B: unplanned, sponteneous"});
+   new Question(new String[]{"A:   seek many tasks public activities, interaction with others", "B: seek private, solitary activities with quiet to concentrate"});
+  new Question(new String[]{"A:   standard, usual, conventional", "B: different, novel, unique"});
+  new Question(new String[]{"A:   firm, tend to criticize, hold the line", "B: gentle, tend to appreciate, conciliate"});
+  new Question(new String[]{"A:   regulated, structured", "B: easygoing, \"live\" and \"let live"});
+  new Question(new String[]{"A:   external, communicative, express yourself", "B: internal, reticent, keep to yourself"});
+  new Question(new String[]{"A:   focus on here and now", "B: look to the future, global perspective, big picture"});
+  new Question(new String[]{"A:   tough-minded, just", "B: tender-hearted, merciful"});
+  new Question(new String[]{"A:   preparation, plan ahead", "B: go with the flow, adapt as you go"});
+  new Question(new String[]{"A:   active, initiate", "B: reflective, deliberate"});
+  new Question(new String[]{"A:   facts, things \"what is\"", "B: ideas, dreams, \"what could be,\" philosophical"});
+  new Question(new String[]{"A:   matter of fact, issue-oriented", "B: sensitive, people-oriented, compassionate"});
+  new Question(new String[]{"A:   control, govern", "B: latitude, freedom"});
+
+    }
+
+    public String [] getOptions(){
+
+
+        return 0;
+    }
+}
+
+
+
