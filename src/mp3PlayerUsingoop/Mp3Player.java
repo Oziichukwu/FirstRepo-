34a4229b83fbf2ebbf3658967aa67a6b43eb5bc4
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-09-26 19:53:51 +0100

    new addition

--- src/mp3PlayerUsingoop/Mp3Player.java
+++ src/mp3PlayerUsingoop/Mp3Player.java
@@ -3,14 +3,33 @@ package mp3PlayerUsingoop;
 import java.util.List;
 
 public class Mp3Player {
+
+    private MusicState musicState = MusicState.STOP;
+
+    private Playlist playlist = new Playlist();
+
     private boolean isOn;
+
+    public Playlist getPlaylist() {
+
+        return  playlist;
+    }
+
     public void setOn(boolean isOn) {
 
         this.isOn = isOn;
     }
 
-    public boolean getIson() {
+    public boolean getIsOn() {
         return isOn;
+    }
+
+    public void setMusicState(MusicState currentMusicState){
+
+        musicState = currentMusicState;
+    }
+    public MusicState getMusicState(){
+        return musicState;
     }
 
 //private final List<Playlist>playlistCollection;
