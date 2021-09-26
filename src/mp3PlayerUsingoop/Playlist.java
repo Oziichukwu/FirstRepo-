953bf3250aec9ac5c624fbeca93cfeb8bb6f247f
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-09-26 16:38:22 +0100

    new mp3 player

--- src/mp3PlayerUsingoop/Playlist.java
+++ src/mp3PlayerUsingoop/Playlist.java
@@ -1,2 +1,12 @@
-package mp3PlayerUsingoop;public class Playlist {
+package mp3PlayerUsingoop;
+
+import java.util.List;
+
+public class Playlist {
+
+    private final List<Music>collectionOfMusic;
+
+    public Playlist(List<Music> collectionOfMusic) {
+        this.collectionOfMusic = collectionOfMusic;
+    }
 }
