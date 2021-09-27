df8f284ad02e92fb6880b4f375a5f0f2509b4428
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-09-27 20:43:42 +0100

    files added

--- src/chapterSeven/tutleGraphics/Position.java
+++ src/chapterSeven/tutleGraphics/Position.java
@@ -1,2 +1,71 @@
-package chapterSeven.tutleGraphics;public class Position {
+package chapterSeven.tutleGraphics;
+
+import java.util.Objects;
+
+public class Position {
+    private int rowPosition;
+    private int columnPosition;
+
+    public Position(int rowPosition, int columnPosition) {
+                this.columnPosition = columnPosition;
+                this.rowPosition = rowPosition;
+    }
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        Position position = (Position) o;
+//        return rowPosition == position.rowPosition && columnPosition == position.columnPosition;
+//    }
+
+
+    @Override
+    public String toString() {
+        return "Position{" +
+                "rowPosition=" + rowPosition +
+                ", columnPosition=" + columnPosition +
+                '}';
+    }
+//
+//    @Override
+//    public boolean equals(Object o){
+//
+//        Position comparedPosition = (Position) o;
+//        //if (this == comparedPosition)
+//
+//        if(columnPosition==comparedPosition.columnPosition && rowPosition == comparedPosition.rowPosition)
+//
+//        return false;
+//
+//    }
+
+    @Override
+    public boolean equals(Object o){
+
+        if (this == o){
+            return true;
+        }
+        if (!(o instanceof Position positionToBeCompared)){
+            return false;
+        }
+        boolean rowsAreEqual = rowPosition == positionToBeCompared.rowPosition;
+        boolean columnsAreEqual = columnPosition == positionToBeCompared.columnPosition;
+        return rowsAreEqual && columnsAreEqual;
+    }
+
+    public int getColumnPosition() {
+        return columnPosition;
+    }
+
+    public void setColumnPosition(int columnPosition) {
+        this.columnPosition = columnPosition;
+    }
+
+    public int getRowPosition() {
+        return rowPosition;
+    }
+
+    public void setRowPosition(int rowPosition){
+        this.rowPosition = rowPosition;
+    }
 }
