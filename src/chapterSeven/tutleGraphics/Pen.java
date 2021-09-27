b4dbe0c36d4866b7420a780947161acb77b8507c
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-09-27 20:46:23 +0100

    pen added

--- src/chapterSeven/tutleGraphics/Pen.java
+++ src/chapterSeven/tutleGraphics/Pen.java
@@ -1,2 +1,17 @@
-package chapterSeven.tutleGraphics;public class Pen {
+package chapterSeven.tutleGraphics;
+
+import static chapterSeven.tutleGraphics.PenPosition.UP;
+
+public class Pen {
+    private PenPosition penPosition = UP;
+
+    public PenPosition getPosition() {
+
+        //return PenPosition.UP;
+        return penPosition;
+    }
+
+    public void setPosition(PenPosition currentPosition) {
+        penPosition = currentPosition;
+    }
 }
