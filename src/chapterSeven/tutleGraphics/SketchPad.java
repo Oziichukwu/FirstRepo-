5c0827de8cff0c13174b9ada8f783f1ceae7c2c2
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-09-27 20:49:41 +0100

    new added

--- src/chapterSeven/tutleGraphics/SketchPad.java
+++ src/chapterSeven/tutleGraphics/SketchPad.java
@@ -1,2 +1,28 @@
-package chapterSeven.tutleGraphics;public class sketchPad {
+package chapterSeven.tutleGraphics;
+
+public class SketchPad {
+    int [][]floor;
+    public SketchPad(int floorRow, int floorColumn) {
+        floor = new int [floorRow][floorColumn];
+    }
+
+    public int [][]  getFloor() {
+
+        return floor;
+    }
+
+    public void displayFloor(){
+
+        for (int [] row : floor){
+            for (int column: row){
+                if (column == 1){
+                    System.out.print("# ");
+                }
+                if (column == 0){
+                    System.out.print("00");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
