c49605045b91c417ab6fe74c3137205657f8e5db
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-09-27 20:50:25 +0100

    linear search

--- src/practiseExercises/LinearSearch.java
+++ src/practiseExercises/LinearSearch.java
@@ -1,2 +1,18 @@
-package practiseExercises;public class LinearSearch {
+package practiseExercises;
+
+public class LinearSearch{
+
+    public static void main(String[] args) {
+
+        int [] list = {4,5,3,2,7,5,1};
+
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] == 5){
+                System.out.println("number found at index: "+(i));
+            }
+//            else
+//                System.out.println( (i+1)  + "not found");
+        }
+    }
+
 }
