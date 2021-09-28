41d024876c829b1b4862d97f33a70799eb683da0
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-09-28 13:18:34 +0100

    turtle addition

--- src/chapterSeven/tutleGraphics/Turtle.java
+++ src/chapterSeven/tutleGraphics/Turtle.java
@@ -53,54 +53,48 @@ public class Turtle {
         if (currentDirection == Direction.EAST)
             currentPosition.setColumnPosition(currentPosition.getColumnPosition() + numberOfSteps - 1);
 
-        if (currentDirection == Direction.SOUTH)
+        else if (currentDirection == Direction.SOUTH)
             currentPosition.setRowPosition(currentPosition.getRowPosition() + numberOfSteps - 1);
 
-        if (currentDirection == Direction.WEST)
-            currentPosition.setColumnPosition(currentPosition.getColumnPosition() + numberOfSteps - 1);
+        else if (currentDirection == Direction.WEST)
+            currentPosition.setColumnPosition(currentPosition.getColumnPosition() - (numberOfSteps - 1));
 
-        if (currentDirection == Direction.NORTH)
-            currentPosition.setRowPosition(currentPosition.getRowPosition() + numberOfSteps - 1);
+        else if (currentDirection == Direction.NORTH)
+            currentPosition.setRowPosition(currentPosition.getRowPosition() - (numberOfSteps - 1));
     }
 
 
     public void writeOn(SketchPad sketchPad, int numberOfSteps) {
+                int[][] floor = sketchPad.getFloor();
+                int rowPosition = currentPosition.getRowPosition();
+                int columnPosition = currentPosition.getColumnPosition();
         if (this.pen.getPosition() == PenPosition.DOWN) {
             if (this.currentDirection == Direction.EAST) {
-                int[][] floor = sketchPad.getFloor();
-                int rowPosition = currentPosition.getRowPosition();
-                int columnPosition = currentPosition.getColumnPosition();
                 for (int i = 0; i < numberOfSteps; i++) {
                     floor[rowPosition][columnPosition + i] = 1;
                 }
+            }
 
-            } else if (this.currentDirection == Direction.SOUTH) {
-                int[][] floor = sketchPad.getFloor();
-                int rowPosition = currentPosition.getRowPosition();
-                int columnPosition = currentPosition.getColumnPosition();
+             if (this.currentDirection == Direction.SOUTH) {
                 for (int i = 0; i < numberOfSteps; i++) {
                     floor[rowPosition + i][columnPosition] = 1;
-
                 }
             }
-            else if (this.currentDirection == Direction.WEST) {
-                int[][] floor = sketchPad.getFloor();
-                int rowPosition = currentPosition.getRowPosition();
-                int columnPosition = currentPosition.getColumnPosition();
+             if (this.currentDirection == Direction.WEST) {
                 for (int i = 0; i < numberOfSteps; i++) {
-                    floor[rowPosition][columnPosition + i] = 1;
+                    floor[rowPosition][columnPosition - i] = 1;
                 }
             }
-            else if (this.currentDirection == Direction.NORTH) {
-                int[][] floor = sketchPad.getFloor();
-                int rowPosition = currentPosition.getRowPosition();
-                int columnPosition = currentPosition.getColumnPosition();
+             if (this.currentDirection == Direction.NORTH) {
+//                for (int i = numberOfSteps-1; i > 0; i--) {
+//                    floor[rowPosition - i][columnPosition ] = 1;
                 for (int i = 0; i < numberOfSteps; i++) {
-                    floor[rowPosition + i][columnPosition ] = 1;
 
+                    floor[rowPosition-i][columnPosition]=1;
                 }
+
             }
-            moveForward(numberOfSteps);
         }
+            moveForward(numberOfSteps);
     }
 }
