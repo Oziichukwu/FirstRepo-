0788e6e6f0ea2db97589057c3d2310bcb8f24be4
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-09-29 14:29:40 +0100

    new addition

--- src/practiseExercises/Screen.java
+++ src/practiseExercises/Screen.java
@@ -1,2 +1,11 @@
-package practiseExercises;public class Screen {
+package practiseExercises;
+import static practiseExercises.SevenSegmentDisplay.*;
+
+public class Screen {
+    public static void main(String[] args) {
+
+        collectUserInputs();
+        setCharacterArrayWithUserInput();
+        //display();
+    }
 }
