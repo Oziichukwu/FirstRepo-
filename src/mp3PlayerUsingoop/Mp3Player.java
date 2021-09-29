e4f7ffa5f08c77ee7a9f5805d0a2f7bc493bd98b
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-09-29 14:30:34 +0100

    mp3 player

--- src/mp3PlayerUsingoop/Mp3Player.java
+++ src/mp3PlayerUsingoop/Mp3Player.java
@@ -1,31 +1,20 @@
 package mp3PlayerUsingoop;
 
-import java.security.SecureRandom;
 import java.util.ArrayList;
 
 public class Mp3Player {
 
-private static ArrayList<Music> playlistCollection;
+private static ArrayList<Playlist> playlistCollection;
 
-//    public Mp3Player() {
-//        playlistCollection = new ArrayList<Music>();
-//    }
-    SecureRandom randomNumber = new SecureRandom();
+private static ArrayList<Playlist> playlistCollection2;
 
-    private MusicState musicState = MusicState.STOP;
-
-    private Playlist playlist = new Playlist();
-
+private static ArrayList<Playlist> playlistCollection3;
     private boolean isOn;
 
-    private int shuffleMusic;
-
-    private int volume = 5;
-    private int nextMusic;
-
-    public Playlist getPlaylist() {
-
-        return  playlist;
+    public Mp3Player() {
+        playlistCollection = new ArrayList<>();
+        playlistCollection2 = playlistCollection;
+        playlistCollection3 = playlistCollection;
     }
 
     public void setOn(boolean isOn) {
@@ -37,57 +26,7 @@ private static ArrayList<Music> playlistCollection;
         return isOn;
     }
 
-    public void setMusicState(MusicState currentMusicState){
-
-        musicState = currentMusicState;
-    }
-    public MusicState getMusicState(){
-        return musicState;
-    }
-
-    public void increaseVolumeBy5() {
-        if (volume < 100) {
-            volume = volume + 5;
-        }
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void decreaseVolume() {
-        if(volume > 5)
-        volume = volume - 5;
-    }
-
-    public void Mp3PlayerCanPlayNextMusic() {
-        nextMusic = nextMusic + 1;
-    }
-
-    public int getNextMusic() {
-        return nextMusic;
-    }
-
-    public void playPreviousMusic() {
-        nextMusic = nextMusic - 1;
-    }
-
-
-    public int playMusicAtRandom(SecureRandom randomNumber) {
-
-           return  0;
-    }
-
-    public void addMusicToPlaylist(Music music) {
-           playlistCollection.add(music);
-    }
-
-    public int getSizeOfPlaylist() {
-
-        return playlistCollection.size();
-    }
-
-    public void removeMusicFromPlaylist(Music music) {
-        playlistCollection.remove(music);
+    public ArrayList<Playlist> getPlaylist() {
+        return playlistCollection;
     }
 }
