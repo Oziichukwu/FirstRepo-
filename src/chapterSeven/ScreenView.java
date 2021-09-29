5acaf7b437b2e6a8d31abf0083f0643e3efd1107
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-09-29 14:36:15 +0100

    new addition

--- src/chapterSeven/ScreenView.java
+++ src/chapterSeven/ScreenView.java
@@ -1,2 +1,18 @@
-package chapterSeven;public class ScreenView {
+package chapterSeven;
+
+import java.util.Scanner;
+
+public class ScreenView {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public String colectUserInput(){
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your eight digit numbers");
+        return colectUserInput();
+    }
 }
