0f6ccf36a93f51bdf2c8f1a0f190c53fa5827288
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-09-29 14:34:17 +0100

    display

--- src/practiseExercises/DisplaySegment.java
+++ src/practiseExercises/DisplaySegment.java
@@ -1,2 +1,28 @@
-package practiseExercises;public class DisplaySegment {
+package practiseExercises;
+
+public class DisplaySegment {
+
+    public static void main(String[] args) {
+
+//        char[] characters = {'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'};
+//
+//        for (int i = 1; i < characters.length; i++) {
+//
+//                System.out.print(characters[i]);
+//            if ( i % 4 == 0 ) {
+//                System.out.println();
+//            }
+//        }
+//    }
+
+        String [][] numbers = {{"#","#","#","#"},{"#"," "," ","#"},{"#","#","#","#"},{"#"," "," ","#"},{"#","#","#","#"}};
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers[i].length; j++) {
+
+                System.out.print(" " + numbers[i][j]);
+            }
+            System.out.println();
+        }
+    }
 }
