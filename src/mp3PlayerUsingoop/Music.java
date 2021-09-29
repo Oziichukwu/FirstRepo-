aa6cb0d23fb88b9fd314eaf935b6e6e4c2a86dd7
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-09-29 14:31:19 +0100

    mp3 player

--- src/mp3PlayerUsingoop/Music.java
+++ src/mp3PlayerUsingoop/Music.java
@@ -3,6 +3,31 @@ package mp3PlayerUsingoop;
 public class Music {
 
     private String musicName;
+
+    public String getMusicName() {
+        return musicName;
+    }
+
+    public void setMusicName(String musicName) {
+        this.musicName = musicName;
+    }
+
+    public int getTrackNumber() {
+        return trackNumber;
+    }
+
+    public void setTrackNumber(int trackNumber) {
+        this.trackNumber = trackNumber;
+    }
+
+    public int getTrackLength() {
+        return trackLength;
+    }
+
+    public void setTrackLength(int trackLength) {
+        this.trackLength = trackLength;
+    }
+
     private int trackNumber;
     private int trackLength;
     }
