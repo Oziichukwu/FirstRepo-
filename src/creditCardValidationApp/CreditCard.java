4e31bb53c30bbca14ed74719329a2181cf9d2b71
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-02 23:59:50 +0100

    added new files

--- src/creditCardValidationApp/CreditCard.java
+++ src/creditCardValidationApp/CreditCard.java
@@ -27,14 +27,55 @@ public class CreditCard {
         if (creditCardNumber.length() == 13){
             return true;
         }
-        if (creditCardNumber.length() == 14){
+        if (creditCardNumber.length() == 14 ){
             return true;
         }
-        if (creditCardNumber.length() == 15){
+        if (creditCardNumber.length() == 15 ){
             return true;
         }
         else {
             return false;
         }
     }
+
+    public int sumOfDoubleEvenPlace(String creditCardNumber) {
+        int sumOfEvenNumbers = 0;
+
+        for (int i = creditCardNumber.length()-2; i >= 0; i-=2) {
+
+            int sumOfDoubleEvenPlaceNumbers = doubleEvenPlaceNumbers(creditCardNumber, i);
+
+            if (sumOfDoubleEvenPlaceNumbers > 9){
+                sumOfEvenNumbers += (sumOfDoubleEvenPlaceNumbers/10) + (sumOfDoubleEvenPlaceNumbers % 10);
+            }
+            else {
+                sumOfEvenNumbers += sumOfDoubleEvenPlaceNumbers;
+            }
+        }
+        return sumOfEvenNumbers;
+    }
+
+    private int doubleEvenPlaceNumbers(String creditCardNumber, int i) {
+        int digitValueOfCreditCard = Integer.parseInt(String.valueOf(creditCardNumber.charAt(i)));
+
+        return digitValueOfCreditCard * 2;
+    }
+
+    public int sumOfDoubleOddPlace(String creditCardNumber) {
+        int sumOfOddNumbers = 0;
+
+        for (int i = creditCardNumber.length()-1; i >= 0; i-=2) {
+            int digitValueOfCardNumber = Integer.parseInt(String.valueOf(creditCardNumber.charAt(i)));
+              int sumOfDoubledOddNumbers  = digitValueOfCardNumber * 2;
+              if (sumOfDoubledOddNumbers > 9){
+                  sumOfOddNumbers += (sumOfDoubledOddNumbers / 10) + (sumOfDoubledOddNumbers % 10);
+              }
+              else
+                  sumOfOddNumbers += sumOfDoubledOddNumbers;
+        }
+
+        return sumOfOddNumbers;
+    }
+
+
 }
