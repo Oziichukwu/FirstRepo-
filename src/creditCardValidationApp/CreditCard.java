0e1a0f622e0ae455d69e9ca080bd884d315df6b7
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-02 18:47:49 +0100

    credit card

--- src/creditCardValidationApp/CreditCard.java
+++ src/creditCardValidationApp/CreditCard.java
@@ -1,2 +1,40 @@
-package creditCardValidationApp;public class CreditCard {
+package creditCardValidationApp;
+
+public class CreditCard {
+
+    public boolean validPrefix(String creditCardNumber) {
+        if (creditCardNumber.charAt(0) == '4') {
+            return true;
+        }
+        if (creditCardNumber.charAt(0) == '5') {
+            return true;
+        }
+        if (creditCardNumber.charAt(0) == '3' && creditCardNumber.charAt(1) == '7') {
+            return true;
+        }
+        if (creditCardNumber.charAt(0) == '6') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public boolean validLength(String creditCardNumber) {
+        if (creditCardNumber.length() == 16) {
+            return true;
+        }
+        if (creditCardNumber.length() == 13){
+            return true;
+        }
+        if (creditCardNumber.length() == 14){
+            return true;
+        }
+        if (creditCardNumber.length() == 15){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
