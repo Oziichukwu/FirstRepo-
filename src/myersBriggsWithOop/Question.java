78cf2fe69138dffb36b907206306b89a2c16c08f
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-04 00:48:02 +0100

    oop myersbriggs

--- src/myersBriggsWithOop/Question.java
+++ src/myersBriggsWithOop/Question.java
@@ -1,2 +1,23 @@
-package myersBriggsWithOop;public class Question {
+package myersBriggsWithOop;
+
+public class Question {
+    private String questionValue;
+    private char response;
+
+    public Question(String questionValue, char response) {
+        this.questionValue = questionValue;
+        this.response = response;
+    }
+    public String getQuestionValue() {
+        return questionValue;
+    }
+
+    public char getResponse() {
+        return response;
+    }
+
+    public void setResponse(char response) {
+        this.response = response;
+    }
+
 }
