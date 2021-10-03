f13f509ae7a0fe5ccbe6cce81dc3805feeecd1d1
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-03 07:08:43 +0100

    oop

--- src/designPartterns/strategyPattern/WorkWithAnimals.java
+++ src/designPartterns/strategyPattern/WorkWithAnimals.java
@@ -1,2 +1,27 @@
-package designPartterns.strategyPattern;public class WorkWithAnimals {
-}
+ package designPartterns.strategyPattern;
+
+public class WorkWithAnimals {
+    public static void main(String[] args) {
+
+        Dog puppy = new Dog();
+
+        puppy.setName("My name is Leonardo");
+        System.out.println(puppy.getName());
+
+        puppy.digHole();
+        puppy.setWeight(-1);
+
+        int randNum = 10;
+
+        puppy.changeVar(randNum);
+        System.out.println("ranNum after method call is : " + randNum);
+
+        changeObjectName(puppy);
+        System.out.println("dog name after method call " + puppy.getName());
+    }
+
+    public static void changeObjectName(Dog puppy){
+
+        puppy.setName("Segun");
+    }
+ }
