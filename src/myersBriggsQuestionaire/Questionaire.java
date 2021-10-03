73ad3dac7492f23b8e3a182c271018baec6a305d
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-04 00:49:18 +0100

    file

--- src/myersBriggsQuestionaire/Questionaire.java
+++ src/myersBriggsQuestionaire/Questionaire.java
@@ -1,2 +1,16 @@
-package myersBriggsQuestionaire;public class Questionaire {
+package myersBriggsQuestionaire;
+
+public class Questionaire {
+
+    Question [] questions = new Question[20];
+    Question [] questionsToServe = new Question[20];
+
+    public Questionaire() {
+        this.questions = questions;
+        this.questionsToServe = questionsToServe;
+    }
+
+    public Question[] getQuestions() {
+        return questions;
+    }
 }
