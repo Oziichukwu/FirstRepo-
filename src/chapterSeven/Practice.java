15939c73316c2e9b1d51acdc03393fbab881cfd7
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-03 07:09:38 +0100

    files

--- src/chapterSeven/Practice.java
+++ src/chapterSeven/Practice.java
@@ -1,2 +1,20 @@
-package chapterSeven;public class Practice {
+package chapterSeven;
+
+import java.util.Arrays;
+
+public class Practice {
+
+    public static void main(String[] args) {
+
+        int [] count = new int[10];
+        for (int i = 0; i < count.length; i++) {
+            count[0]++;
+        }
+        System.out.println(count[0]);
+
+        Arrays.fill(count,7);
+        System.out.println(Arrays.toString(count));
+        count = new int[10];
+        System.out.print(Arrays.toString(count));
+    }
 }
