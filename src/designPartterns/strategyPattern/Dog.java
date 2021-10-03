1e18264e55f6e7b8b37e3bf984af1eab7361180d
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-03 07:08:15 +0100

    oop

--- src/designPartterns/strategyPattern/Dog.java
+++ src/designPartterns/strategyPattern/Dog.java
@@ -1,2 +1,21 @@
-package designPartterns.strategyPattern;public class Dog {
+package designPartterns.strategyPattern;
+
+public class Dog extends Animal{
+
+    public void digHole() {
+        System.out.println("Digs a Hole");
+    }
+
+    public Dog(){
+        super();
+
+        setSound("Bark");
+    }
+
+    public void changeVar(int randNum){
+
+        randNum = 12;
+        System.out.println("ranNum in method: " + randNum);
+
+    }
 }
