76555f779e3d4eebab06fb1547bb59162b52081c
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-06 18:01:30 +0100

    strategy pattern

--- src/designPartterns/characterStrategypattern/WeaponBehaviour.java
+++ src/designPartterns/characterStrategypattern/WeaponBehaviour.java
@@ -1,2 +1,35 @@
-package designPartterns.characterStrategypattern;public interface WeaponBehaviour {
+package designPartterns.characterStrategypattern;
+
+public interface WeaponBehaviour {
+    String fight();
 }
+
+class AxeBehaviour implements WeaponBehaviour{
+
+
+    public String fight() {
+        return "I can fight with an Axe";
+    }
+}
+
+class KnifeBehaviour implements WeaponBehaviour{
+
+    public String fight() {
+        return "I can fight perfectly well with a knife";
+    }
+}
+
+class BowAndArrowBehaviour implements WeaponBehaviour{
+
+    public String fight() {
+        return "Bow and arrows are my favourite weapons to fight";
+    }
+}
+
+class SwordBehaviour implements WeaponBehaviour{
+
+    public String fight() {
+        return "Swords are the best for defense against enemies";
+    }
+}
+
