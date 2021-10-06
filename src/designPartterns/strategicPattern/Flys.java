3dbe59d8a433d4a67503b75fb98afa6e7ad403c6
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-06 18:02:03 +0100

    strategy pattern

--- src/designPartterns/strategicPattern/Flys.java
+++ src/designPartterns/strategicPattern/Flys.java
@@ -1,2 +1,18 @@
-package designPartterns.strategicPattern;public interface Flys {
+package designPartterns.strategicPattern;
+
+public interface Flys {
+    String fly();
+}
+
+class ItFlys implements Flys{
+
+    public String fly() {
+        return "It flys soooo High";
+    }
+}
+class CantFly implements Flys{
+
+    public String fly() {
+        return "Absolutely cannot fly";
+    }
 }
