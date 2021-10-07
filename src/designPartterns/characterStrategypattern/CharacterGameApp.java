4ae4b1685e369b28a13000f1476fcb1d7e846461
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-07 11:51:35 +0100

    inheritance

--- src/designPartterns/characterStrategypattern/CharacterGameApp.java
+++ src/designPartterns/characterStrategypattern/CharacterGameApp.java
@@ -1,2 +1,19 @@
-package designPartterns.characterStrategypattern;public class CharacterGameApp {
+package designPartterns.characterStrategypattern;
+
+public class CharacterGameApp {
+
+    public static void main(String[] args) {
+
+        Character elizabeth = new Queen();
+        Character john = new King();
+        Character william = new Troll();
+
+        System.out.println("Queen: " +  elizabeth.displayFightPattern());
+        System.out.println("King: " +  john.displayFightPattern());
+        System.out.println("Troll: " +  william.displayFightPattern());
+
+        Character willy = new Troll();
+        willy.setFightCharacters(new AxeBehaviour());
+        System.out.println("Troll: " + willy.displayFightPattern());
+    }
 }
