d6d4ba473a6a3f28b29ea295b36be87c04cc8363
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-07 11:46:57 +0100

    inheritance

--- src/designPartterns/strategyPattern/Bird.java
+++ src/designPartterns/strategyPattern/Bird.java
@@ -1,5 +1,13 @@
 package designPartterns.strategyPattern;
 
-public class Bird {
+public class Bird extends Animal{
 
+    public Bird(){
+
+        super();
+
+        setSound("Tweet");
+
+        flyingType = new Itflys();
+    }
 }
