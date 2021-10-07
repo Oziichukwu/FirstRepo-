2073bd238930626c435516601721a1c9203ac961
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-07 11:52:59 +0100

    inheritance

--- src/designPartterns/strategyPattern/Creature.java
+++ src/designPartterns/strategyPattern/Creature.java
@@ -1,2 +1,15 @@
-package designPartterns.strategyPattern;public class Creature {
+package designPartterns.strategyPattern;
+
+abstract public class Creature {
+
+     protected String name;
+     protected int weight;
+     protected String sound;
+
+
+     public abstract void setWeight(double newWeight);
+     public abstract double getWeight();
+
+     public abstract void setName(String newName);
+     public abstract String getName();
 }
