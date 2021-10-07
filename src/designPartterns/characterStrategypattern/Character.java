63715a9ad12cb24a29ebf7cf119ac41efe2b22b0
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-07 11:52:31 +0100

    inheritance

--- src/designPartterns/characterStrategypattern/Character.java
+++ src/designPartterns/characterStrategypattern/Character.java
@@ -1,2 +1,16 @@
-package designPartterns.characterStrategypattern;public class Character {
+package designPartterns.characterStrategypattern;
+
+public class Character {
+
+    public WeaponBehaviour fightCharacters;
+
+
+    public String displayFightPattern(){
+
+        return  fightCharacters.fight();
+    }
+
+    public void setFightCharacters(WeaponBehaviour newFightCharacter){
+        fightCharacters = newFightCharacter;
+    }
 }
