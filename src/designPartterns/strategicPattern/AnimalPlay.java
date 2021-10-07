abe576b633d6082e1f509b930ce26ce0b36d6846
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-07 11:49:36 +0100

    inheritance

--- src/designPartterns/strategicPattern/AnimalPlay.java
+++ src/designPartterns/strategicPattern/AnimalPlay.java
@@ -1,2 +1,16 @@
-package designPartterns.strategicPattern;public class AnimalPlay {
+package designPartterns.strategicPattern;
+
+public class AnimalPlay {
+    public static void main(String[] args) {
+
+        Animal  doggy = new Dog();
+        Animal tweety = new Bird();
+
+        System.out.println("dog: " + doggy.tryToFly());
+        System.out.println("bird: " + tweety.tryToFly());
+
+        doggy.setFlyingAbility(new ItFlys() );
+        System.out.println("dog: " + doggy.tryToFly());
+
+    }
 }
