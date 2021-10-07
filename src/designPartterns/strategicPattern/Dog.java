adedfc129a1dcf7836cc40661913943329a5bcea
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-07 11:48:39 +0100

    inheritance

--- src/designPartterns/strategicPattern/Dog.java
+++ src/designPartterns/strategicPattern/Dog.java
@@ -1,2 +1,15 @@
-package designPartterns.strategicPattern;public class Dog {
+package designPartterns.strategicPattern;
+
+public class Dog extends Animal{
+    public void digHole() {
+        System.out.println("Digs a Hole");
+    }
+
+    public Dog() {
+        super();
+
+        setSound("Bark");
+
+        flyingType = new CantFly();
+    }
 }
