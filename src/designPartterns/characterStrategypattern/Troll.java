5f5416ddd11ae980ae13a7fca78d142fe0f46463
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-07 11:44:28 +0100

    inheritance

--- src/designPartterns/characterStrategypattern/Troll.java
+++ src/designPartterns/characterStrategypattern/Troll.java
@@ -1,2 +1,11 @@
-package designPartterns.characterStrategypattern;public class Troll {
+package designPartterns.characterStrategypattern;
+
+public class Troll extends Character{
+
+    public Troll(){
+
+        super();
+
+        fightCharacters = new KnifeBehaviour();
+    }
 }
