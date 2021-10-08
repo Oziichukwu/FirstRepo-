0a1839530e7e68cd2722744a4229ebb55bdf4cfe
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-08 09:41:46 +0100

    inheritance

--- src/designPartterns/strategyPattern/Flys.java
+++ src/designPartterns/strategyPattern/Flys.java
@@ -1,2 +1,21 @@
-package designPartterns.strategyPattern;public interface Flys {
+package designPartterns.strategyPattern;
+
+public interface Flys {
+
+    String fly();
 }
+
+class Itflys implements Flys {
+
+    public String fly() {
+        return "flying sooo high";
+    }
+}
+    class CantFly implements Flys{
+
+        public String fly() {
+            return "i cant fly";
+        }
+    }
+
+
