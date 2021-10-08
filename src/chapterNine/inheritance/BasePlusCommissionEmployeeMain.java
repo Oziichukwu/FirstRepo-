38e9ceaae5914d084eaddc02cd9d30034d5d785d
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-08 09:39:48 +0100

    add

--- src/chapterNine/inheritance/BasePlusCommissionEmployeeMain.java
+++ src/chapterNine/inheritance/BasePlusCommissionEmployeeMain.java
@@ -1,2 +1,11 @@
-package chapterNine.inheritance;public class BasePlusCommissionEmployeeMain {
+package chapterNine.inheritance;
+
+public class BasePlusCommissionEmployeeMain {
+
+    public static void main(String[] args) {
+
+    BasePlusCommissionEmployee basePlusCommissionEmployee = new BasePlusCommissionEmployee("john","okonkwo","333-33-333", 3000,0.04,500);
+
+    System.out.printf("%n%s: %n%n%s%n", "Employee information", basePlusCommissionEmployee);
+    }
 }
