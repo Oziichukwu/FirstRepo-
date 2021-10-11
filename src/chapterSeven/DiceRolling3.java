14687a602740956c931a0aaf006a7f13c1aceca6
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-11 13:07:30 +0100

    dice rolling

--- src/chapterSeven/DiceRolling3.java
+++ src/chapterSeven/DiceRolling3.java
@@ -1,2 +1,29 @@
-package chapterSeven;public class DiceRolling3 {
+package chapterSeven;
+
+import java.security.SecureRandom;
+
+public class DiceRolling3 {
+
+    private  static final SecureRandom random = new SecureRandom();
+
+    public static void main(String[] args) {
+
+        int [] frequency = new int[13];
+
+        for (int i = 0; i < 36; i++) {
+
+            int die1 = 1 + random.nextInt(6);
+            int die2 = 1 + random.nextInt(6);
+
+            int sum = die1 + die2;
+
+            ++frequency[sum];
+        }
+
+        System.out.printf("%s %s %n", "sum", "frequency");
+
+        for (int i = 0; i < frequency.length; i++) {
+            System.out.printf("%2d %10d%n", i, frequency[i]);
+        }
+    }
 }
