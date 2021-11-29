689698a13155e8a41cfccbf9298245907b6c6d5a
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-29 09:22:22 +0100

    additions

--- src/chapterSeventeen/StreamReduce.java
+++ src/chapterSeventeen/StreamReduce.java
@@ -1,2 +1,13 @@
-package chapterSeventeen;public class StreamReduce {
+package chapterSeventeen;
+
+import java.util.stream.IntStream;
+
+public class StreamReduce {
+    public static void main(String[] args) {
+
+        System.out.printf("sum of 1 through 10 is:  %d%n",
+
+        IntStream.rangeClosed(1,10)
+                .sum());
+    }
 }
--- src/chapterSixteen/Stack.java
+++ src/chapterSixteen/Stack.java
@@ -2,5 +2,55 @@ package chapterSixteen;
 
 public class Stack {
 
+    private int numberOfElement;
+    private int[] elements;
+
+    public Stack(int capacity) {
+        elements = new int[capacity];
+    }
+
+    public void add(int element) {
+        //if (isFull()) throw new UnderFlowException("Stack is full");
+        elements[numberOfElement++] = element ;
+        //numberOfElement++;
+    }
+
+    public int size() {
+        return numberOfElement;
+    }
+
+    public void pop() {
+        if (isEmpty()) throw new UnderFlowException("Stack is empty");
+        --numberOfElement;
+    }
+
+    public boolean isEmpty() {
+
+        return numberOfElement == 0;
+    }
+
+    public int peek() {
+
+        return elements[numberOfElement-1];
+    }
+//
+//    public boolean isFull() {
+//
+//        return numberOfElement;
+//    }
+
+    public static class UnderFlowException extends  RuntimeException{
+        public UnderFlowException(String message){
+            super(message);
+        }
+
+    }
+
+    public static class OverFlowException extends  RuntimeException{
+        public OverFlowException(String message){
+            super(message);
+        }
+
+    }
 
 }
