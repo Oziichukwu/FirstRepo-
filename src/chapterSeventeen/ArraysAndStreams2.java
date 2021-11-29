815b83af9b93f5ce3262addda5a9dae7a51bf2ed
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-29 09:24:43 +0100

    files and file streams

--- src/chapterSeventeen/ArraysAndStreams2.java
+++ src/chapterSeventeen/ArraysAndStreams2.java
@@ -1,2 +1,30 @@
-package chapterSeventeen;public class ArraysAndStreams2 {
+package chapterSeventeen;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class ArraysAndStreams2 {
+
+    public static void main(String[] args) {
+
+        String [] strings = {"Red", "orange", "Yellow", "green", "Blue", "indigo", "Violet"};
+
+        System.out.printf("Original strings: %s%n", Arrays.asList(strings));
+
+        System.out.printf("strings in uppercase: %s%n",
+                Arrays.stream(strings)
+                        .map(String::toUpperCase)
+                        .collect(Collectors.toList()));
+
+        System.out.printf("strings in uppercase: %s%n",
+                Arrays.stream(strings)
+                        .filter(s -> s.compareToIgnoreCase("n")< 0)
+                        .collect(Collectors.toList()));
+
+        System.out.printf("strings greater than m sorted descending: %s%n",
+                Arrays.stream(strings)
+                        .filter(s -> s.compareToIgnoreCase("n")< 0)
+                        .sorted(String.CASE_INSENSITIVE_ORDER.reversed())
+                        .collect(Collectors.toList()));
+    }
 }
