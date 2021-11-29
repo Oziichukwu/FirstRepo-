e44cfdb68b609bc15c71fb3ca3c3d8a062eaad94
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-29 09:24:02 +0100

    files

--- src/chapterSeventeen/RandomIntegers.java
+++ src/chapterSeventeen/RandomIntegers.java
@@ -1,2 +1,22 @@
-package chapterSeventeen;public class RandomIntegers {
+package chapterSeventeen;
+
+import java.security.SecureRandom;
+import java.util.stream.Collectors;
+
+public class RandomIntegers {
+
+    public static void main(String[] args) {
+
+        SecureRandom randomNumber = new SecureRandom();
+
+        System.out.println("Random numbers on separate lines:");
+        randomNumber.ints(10,1,7)
+                .forEach(System.out::println);
+
+        String numbers =
+                randomNumber.ints(10,1,7)
+                        .mapToObj(String::valueOf)
+                        .collect(Collectors.joining(" "));
+        System.out.printf("%nRandom numbers on one line: %s%n", numbers);
+    }
 }
