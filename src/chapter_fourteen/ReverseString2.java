fc578d652c32b7c2f11251f11bfadfa1fbdfc605
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-29 09:21:53 +0100

    regex

--- src/chapter_fourteen/ReverseString2.java
+++ src/chapter_fourteen/ReverseString2.java
@@ -1,2 +1,21 @@
-package chapter_fourteen;public class ReverseString2 {
+package chapter_fourteen;
+
+public class ReverseString2 {
+
+    public static void main(String[] args) {
+        String letters = "Do you beleive you can fly";
+        System.out.println(reverseStringTwo(letters));
+    }
+
+    public static String reverseStringTwo(String letters){
+
+        char [] charArray = letters.toCharArray();
+        char temp;
+        for (int i = 0, j = charArray.length-1; i < j; i++, j--) {
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+        }
+        return new String(charArray);
+    }
 }
--- src/practiseExercises/Stack.java
+++ src/practiseExercises/Stack.java
@@ -1,2 +1,67 @@
-package practiseExercises;public class Stacki {
+package practiseExercises;
+
+public class Stack {
+
+    private int maximumSize;
+    private int[] stackArray;
+    private int topOfStack ;
+
+    public Stack(int size) {
+        this.maximumSize = size;
+        topOfStack = -1;
+        stackArray = new int[size];
+    }
+
+    public int maximumSize() {
+
+        return maximumSize;
+    }
+
+    public void push(int itemToAdd) throws Exception{
+
+        if(!isFull()){
+            stackArray[topOfStack + 1] = itemToAdd;
+            topOfStack++;
+        }else{
+            throw new Exception("stack is currently full and cannot push more items");
+        }
+
+    }
+
+    public boolean isFull() {
+
+        int maximumIndex = maximumSize -1;
+        if (topOfStack == maximumIndex){
+            return true;
+        }
+        return false;
+    }
+
+    public int size() {
+        return topOfStack + 1;
+    }
+
+    public int pop() throws Exception{
+        if (!isEmpty()){
+            int itemToAdd = stackArray[topOfStack];
+            stackArray[topOfStack] = 0;
+            topOfStack--;
+            return itemToAdd;
+        }else {
+            throw new Exception("stack is currently and cannot pop more item");
+        }
+    }
+
+    public boolean isEmpty() {
+
+        if (topOfStack == -1){
+            return true;
+        }
+        return false;
+    }
+
+    public int peek() {
+
+        return stackArray[topOfStack];
+    }
 }
