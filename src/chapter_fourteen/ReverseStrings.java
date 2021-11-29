2bdd260ca653fea16eec1563ccffe995791e923c
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-29 09:21:11 +0100

    additions

--- src/chapter_fourteen/ReverseStrings.java
+++ src/chapter_fourteen/ReverseStrings.java
@@ -1,2 +1,16 @@
-package chapter_fourteen;public class ReverseStrings {
+package chapter_fourteen;
+
+public class ReverseStrings {
+
+    public static void main(String[] args) {
+
+        String letters = "i believe i can fly";
+
+        System.out.println(reverseString(letters));
+    }
+
+    public static String reverseString(String letters){
+
+        return new StringBuilder(letters).reverse().toString();
+    }
 }
