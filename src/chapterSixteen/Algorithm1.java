753aa6dd9d30c7912a494410f5dc5ef28938bf98
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-12-17 19:58:14 +0100

    working with collections

--- src/chapterSixteen/Algorithm1.java
+++ src/chapterSixteen/Algorithm1.java
@@ -1,2 +1,42 @@
-package chapterSixteen;public class Algorithm1 {
+package chapterSixteen;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Algorithm1 {
+
+    public static void main(String[] args) {
+
+        Character [] letters = {'P', 'C', 'M'};
+
+        List<Character> list = Arrays.asList(letters);
+
+        System.out.println("list contains: ");
+
+        System.out.printf("%s ", list);
+
+        System.out.println();
+
+        Collections.reverse(list);
+        System.out.printf("%s ", list);
+
+        System.out.println();
+
+        Character [] letterCharacter = new Character[3];
+        List<Character> copyList = Arrays.asList(letterCharacter);
+
+        Collections.copy(copyList, list);
+        System.out.printf("%s ", copyList);
+
+        System.out.println();
+
+        Collections.fill(list, 'T');
+        System.out.printf("%s ", list);
+
+    }
+
+//    private static void output(List<Character> list) {
+//
+//    }
 }
--- src/chapterSixteen/LinkedListTest.java
+++ src/chapterSixteen/LinkedListTest.java
@@ -1,2 +1,69 @@
-package chapterSixteen;public class LinkedListTest {
+package chapterSixteen;
+
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.UpperCase;
+
+import java.util.*;
+
+public class LinkedListTest {
+
+    public static void main(String[] args) {
+
+        String [] colors = {"black", "yellow", "green", "blue", "violet", "silver"};
+
+        List<String> list1 = new LinkedList<>(Arrays.asList(colors));
+
+        String [] colors2 = {"gold", "white", "brown", "blue", "gray", "silver"};
+
+        List<String> list2 = new LinkedList<>(Arrays.asList(colors2));
+
+        list1.addAll(list2);
+        list2 = null;
+
+        printList(list1);
+
+        convertToUpperCaseStrings(list1);
+
+        System.out.printf("%nDeleting elements 4 to 6...");
+        removeItems(list1, 4, 7);
+
+        printList(list1);
+
+        printReversedList(list1);
+
+    }
+
+    private static void removeItems(List<String> list1, int start, int end) {
+
+        list1.subList(start, end).clear();
+    }
+
+    private static void printReversedList(List<String> list1) {
+
+        ListIterator<String> iterator = list1.listIterator(list1.size());
+
+        System.out.printf("%nReversed List:%n");
+        while(iterator.hasPrevious()){
+            System.out.printf("%s ", iterator.previous());
+        }
+    }
+
+    private static void convertToUpperCaseStrings(List<String> list1) {
+        ListIterator<String> iterator = list1.listIterator();
+
+        while (iterator.hasNext()){
+            String color = iterator.next();
+            iterator.set(color.toUpperCase());
+        }
+    }
+
+    private static void printList(List<String> list1) {
+
+        System.out.printf("%nlist:%n");
+
+        for (String color : list1){
+            System.out.printf("%s ", color);
+        }
+
+        System.out.println();
+    }
 }
