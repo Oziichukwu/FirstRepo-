06f4e994f67859640549ede48582928b433bdc6e
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-12-17 19:57:35 +0100

    working on collections

--- src/chapterSixteen/SortElementInReverseOrder.java
+++ src/chapterSixteen/SortElementInReverseOrder.java
@@ -1,2 +1,23 @@
-package chapterSixteen;public class SortElementInReverseOrder {
+package chapterSixteen;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class SortElementInReverseOrder {
+
+    public static void main(String[] args) {
+
+
+    String [] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+
+    List<String> myList = Arrays.asList(suits);
+
+    Collections.sort(myList, Collections.reverseOrder());
+
+        System.out.printf("Sorted Elements in reversed order: %s%n", myList);
+
+    }
+
 }
--- src/chapterSixteen/SortingInAscendingOrder.java
+++ src/chapterSixteen/SortingInAscendingOrder.java
@@ -1,2 +1,19 @@
-package chapterSixteen;public class SortingInAscendingOrder {
+package chapterSixteen;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class SortingInAscendingOrder {
+
+    public static void main(String[] args) {
+
+        String [] suits = {"Hearts", "Diamonds", "Clubs", "Spade"};
+
+        List<String> list = Arrays.asList(suits);
+
+        Collections.sort(list);
+
+        System.out.printf("Sorted Array Elements: %s%n ", list);
+    }
 }
