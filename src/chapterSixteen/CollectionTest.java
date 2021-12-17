785e183bd365bcd97655f6b7e8b1b6c6abe40227
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-12-17 19:56:51 +0100

    working on the colection class

--- src/chapterSixteen/CollectionTest.java
+++ src/chapterSixteen/CollectionTest.java
@@ -1,2 +1,41 @@
-package chapterSixteen;public class CollectionTest {
+package chapterSixteen;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class CollectionTest {
+    public static void main(String[] args) {
+
+
+        String[] colors = {"red", "blue", "orange", "pink", "yellow", "gold"};
+
+        List<String> list = new ArrayList<>(Arrays.asList(colors));
+
+        String [] removeColors = {"red", "orange", "pink"};
+
+       List<String> removeList = new ArrayList<>(Arrays.asList(removeColors));
+
+        System.out.println("ArrayList: ");
+
+        for (String color : list){
+            System.out.printf("%s ", color);
+        }
+
+        removeColor(list, removeList);
+
+        System.out.printf("%n%nArrayList after calling removeColors:%n");
+
+        for (String color : list){
+            System.out.printf("%s ", color);
+        }
+    }
+
+    private static void removeColor(List<String> list, List<String> removeList) {
+
+        list.removeIf(removeList::contains);
+
+    }
+
 }
